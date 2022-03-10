e2fd6c4d855cc26f2b0252e371c59ad7e8b7650b
JuanFe1221 <Cybernetjf64@gmail.com>
2022-03-10 00:27:48 -0500

    cambios en el carrito de compras

--- js/5_Ejer_Carrito_compra.js
+++ js/5_Ejer_Carrito_compra.js
@@ -1,5 +1,87 @@
 "use-strict"
  
+    let costo_producto = 266000
+
+function aumentarCantProducto (){
+
+    aumentarCantProductoInput()
+    costoProducto()
+
+    let cantidad_producto = document.getElementById("can_Productos").innerHTML;
+    let cantidad_productoInt = parseInt(cantidad_producto);
+
+    if ( cantidad_producto <10) {
+        /* alert("subir maldito") */
+        document.getElementById("can_Productos").innerHTML= cantidad_productoInt + 1
+    }
+        
+}
+
+function aumentarCantProductoInput (){
+
+    let cantidad_producto1 = document.getElementById("cantidad_producto").value;
+    let cantidad_productoInt2 = parseInt(cantidad_producto1);
+    
+    if ( cantidad_producto1 <10) {
+            /*  alert("subir maldito")  */
+        document.getElementById("cantidad_producto").value = cantidad_productoInt2 + 1
+    }
+            
+}
+
+function costoProducto (){ 
+    let cantidad_producto = document.getElementById("cantidad_producto").value;
+    let subtotal = costo_producto * Number(cantidad_producto);
+    /* let finSubtotal = new Intl.NumberFormat('es-ES').format(subtotal); */
+    document.getElementById("costo_producto").innerHTML = subtotal
+    /* alert("holis") */
+}
+
+ 
+
+
+
+function disminuirCantProducto (){
+
+    disminuirCantProductoInput()
+    costoProducto()
+
+    let cantidad_producto = document.getElementById("can_Productos").innerHTML;
+    let cantidad_productoInt = parseInt(cantidad_producto);
+     
+    if (cantidad_producto > 0){
+      /*   alert("restar maldito") */
+        document.getElementById("can_Productos").innerHTML = cantidad_productoInt - 1 + "s"
+    }
+}
+
+function disminuirCantProductoInput (){
+
+    let cantidad_producto = document.getElementById("cantidad_producto").value;
+    let cantidad_productoInt = parseInt(cantidad_producto);
+     
+    if (cantidad_producto > 0){
+      /*   alert("restar maldito") */
+        document.getElementById("cantidad_producto").value = cantidad_productoInt - 1 
+    }
+}
+
+
+/* function elininarProducto(){
+    
+    let cantidad_Producto = document.getElementById("cantidad_producto").value;
+    let cantidad_productoInt = parseInt(cantidad_Producto);
+
+    if (cantidad_Producto >0 ) {
+    let eliminarContador = cantidad_productoInt * 0
+    document.getElementById("cantidad_producto").innerHTML = eliminarContador
+    eliminarContador.innerHTML
+    alert("hola")
+}
+
+} */
+
+/*  
 let cantidadProducto 
 let sumarProducto = "";
 let restarProductos = "";
@@ -36,4 +118,6 @@ function funRestarProducto (){
     }
     
 }
+ */
+
 
